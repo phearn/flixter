21dd1821be245bfe9eccc6c2f684fff6c29e8774
Philip Hearn <zero.zain@gmail.com>
2017-03-10 04:55:49 -0500

    Lesson 22. Added ability to upload images to course page for instructors. Configured views for instructors and users. Installed and configured carrierwave and hooked it up with figaro and fog and used AWS as storage.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221224829) do
+ActiveRecord::Schema.define(version: 20170310090311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170221224829) do
     t.text     "description"
     t.decimal  "cost"
     t.integer  "user_id"
+    t.string   "image"
     t.index ["user_id"], name: "index_courses_on_user_id", using: :btree
   end
 
