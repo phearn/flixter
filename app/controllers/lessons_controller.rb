7f6dd041384fa004e1c6b50ab1db5b1bb403b72b
Philip Hearn <zero.zain@gmail.com>
2017-03-16 00:17:17 -0400

    Lesson 28. added user verification to determine if they are enrolled in a course.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,21 @@
 class LessonsController < ApplicationController
-	def show
-	end
+  before_action :authenticate_user!
+  before_action :require_course_enrollment_for_lesson, only: :show
 
-	private
+  def show
+  end
 
-	helper_method :current_lesson
-	def current_lesson
-		@current_lesson ||= Lesson.find(params[:id])
-	end
+  private
+  def require_course_enrollment_for_lesson
+    this_course = current_lesson.section.course
+
+    if current_user.enrolled_in?(this_course) != true
+      redirect_to course_path(this_course), alert: 'Please enroll in this course to view its lessons'
+    end
+  end
+
+  helper_method :current_lesson
+  def current_lesson
+    @current_lesson ||= Lesson.find(params[:id])
+  end
 end
